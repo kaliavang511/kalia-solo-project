999f581e10138f96b317b05de1b5921d16694c04
Kalia Vang <kaliavang511@gmail.com>
2024-06-28 15:53:25 -0500

    got rid of uneeded stuff

--- src/redux/reducers/tribute.reducer.js
+++ src/redux/reducers/tribute.reducer.js
@@ -2,30 +2,10 @@ const tributeReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_TRIBUTE_ITEM":
       return action.payload;
-      case "EDIT_TRIBUTE":
-  return state.map(item =>
-    item.id === action.payload.id 
-      ? { 
-          ...item, 
-          first_name: action.payload.firstName, 
-          middle_name: action.payload.middleName,
-          last_name: action.payload.lastName,
-          obituary: action.payload.obituary,
-          image: action.payload.image,
-          video: action.payload.video,
-          date_of_birth: action.payload.dateOfBirth,
-          date_of_death: action.payload.dateOfDeath
-        } 
-      : item
-  );
-
-    case 'ADD_TRIBUTE_SUCCESS':
-        return [...state, action.payload]; 
-    default:
-        return state;
-}
+      default:
+        return state
   }
 
-
+}
 
 export default tributeReducer;
