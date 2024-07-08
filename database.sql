838e5ec99c63fff000b0e9781c60a5f1769a27fe
Kalia Vang <kaliavang511@gmail.com>
2024-07-07 22:57:40 -0500

    changed qr page

--- database.sql
+++ database.sql
@@ -2,8 +2,23 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+-- Create users table
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR(80) UNIQUE NOT NULL,
+    "password" VARCHAR(1000) NOT NULL
+);
+
+-- Create Tribute table
+CREATE TABLE "tribute" (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES "user"(id),
+    First_Name VARCHAR(200),
+    Middle_Name VARCHAR,
+    Last_Name VARCHAR,
+    obituary VARCHAR,
+    image VARCHAR(300),
+    video VARCHAR(300),
+    Date_of_birth VARCHAR(20),
+    Date_of_death VARCHAR(20)
 );
