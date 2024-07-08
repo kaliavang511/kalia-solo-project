98ca7bd5789e61f15f6dffe04f592e9d98b609c2
Kalia Vang <kaliavang511@gmail.com>
2024-07-07 22:42:40 -0500

    changed url

--- src/components/QrCode/QrCode.jsx
+++ src/components/QrCode/QrCode.jsx
@@ -4,7 +4,7 @@ import './QrCode.css';
 
 
 function QrCode() {
-  const tributePageUrl = `http://localhost:5173/#/tributePageCon`
+  const tributePageUrl = `https://qr-code-for-gravestones-90b039d8fe07.herokuapp.com/#/tributePage`
   return (
     <>
       <h1 className='qrTitle'>QR Code ready</h1>
